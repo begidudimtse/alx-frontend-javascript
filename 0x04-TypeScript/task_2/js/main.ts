a43e68245649ee99242923c22d13f98a99a7b262
begidudimtse <begidudimtse@gmail.com>
2025-09-27 00:49:34 +0300

    advanced types edited

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -37,7 +37,7 @@ class Teacher implements TeacherInterface {
 
 // Function to create either a Director or Teacher
 function createEmployee(salary: number | string): Director | Teacher {
-    if (typeof salary === "number" && salary < 500) {
+    if (Number (salary) < 500) {
         return new Teacher();
     } 
     return new Director();
