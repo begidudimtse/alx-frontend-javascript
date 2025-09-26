ad00c00b2dc92d421b3bb0269b75b8e58b735d4f
begidudimtse <begidudimtse@gmail.com>
2025-09-26 16:49:48 +0300

    build a Teacher interface

---
+++ task_1/js/main.ts
@@ -0,0 +1,21 @@
+// Define the Student interface
+interface Employee {
+  readonly firstName: string;        // only set when initializing
+  readonly lastName: string;         // only set when initializing
+  fullTimeEmployee: boolean;         // always defined
+  yearsOfExperience?: number;        // optional
+  location: string;                  // always defined
+  [key: string]: any;                // allows extra properties dynamically
+}
+
+// Create two students
+const teacher1: Employee = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher1)
+
