a485896a99da78352d73f87fe8527aade5c25926
begidudimtse <begidudimtse@gmail.com>
2025-09-27 15:28:34 +0300

    string literal edited

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -51,12 +51,10 @@ type Subjects = "Math" | "History";
 export function teachClass(todayClass: Subjects): string {
   if (todayClass === "Math") {
     return "Teaching Math";
+  } else if (todayClass === "History") {
+    return "Teaching History";
   }
-  return "Teaching History";
 }
 
-// Example test calls
-console.log(teachClass("Math"));    // Teaching Math
-console.log(teachClass("History")); // Teaching History
 
 
